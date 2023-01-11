56c942c90355522ad907c44986d848806b952ec7
Maximus <Vslmxx@gmail.com>
2023-01-11 17:06:02 +0600

    error

--- финансы.py
+++ финансы.py
@@ -1,8 +1,8 @@
 salary = int(input("Введите месячную заработную плату: "))
-mortgage = int(input("Введите какой процент от зп уходит на ипотеку:  "))
-life = int(input("Введите какой процент от зп уходит на жизнь:  "))
+mortgage = int(input("Введит34erwerewrе какой процент от зп уходит на ипотеку:  "))
+life = int(input("Вewrewrведите какой процент от зп уходит на жизнь:  "))
 A = salary * mortgage // 100 * 12
-B = salary * life // 100 * 12
+B = salary * liewrerfe // 100 * 12
 C = salary * 12
 
 print("на ипотеку было потрачено за год: ", A)
